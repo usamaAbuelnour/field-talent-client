a22f634085d4804e781f8e0bcaf1d60761effb64
fatma-mohamed99 <fatma.99.mph@gmail.com>
2024-09-13 05:36:40 +0300

    edit some style

--- src/components/ShowJobCard.jsx
+++ src/components/ShowJobCard.jsx
@@ -3,23 +3,25 @@ import { MapPin } from 'lucide-react';
 
 const ShowJobCard = ({ job }) => {
   return (
-    <div className="w-full p-4 mb-4 mx-auto rounded-lg shadow-md bg-gray-100">
-      <h2 className="font-bold mb-4 text-center p-2 mx-auto w-full max-w-xs sm:max-w-sm md:max-w-md rounded-lg bg-main text-white text-base sm:text-lg md:text-xl break-words">
+    <div className="w-full px-2 sm:px-4 md:px-10  py-2 sm:py-11 mb-6 sm:mb-8 mx-auto rounded-lg shadow-md bg-gray-100 border border-main border-opacity-30 lg:px-24">
+      <h2 className="font-bold mb-2 sm:mb-4 text-center p-1 sm:p-2 mx-auto w-full max-w-xs sm:max-w-sm md:max-w-md rounded-lg text-main text-sm sm:text-base md:text-lg lg:text-xl break-words">
         {job.title}
       </h2>
-      <p className="mb-4 p-4 bg-white rounded-md text-base sm:text-sm md:text-lg">
+      <p className="mb-2 sm:mb-4 p-2 sm:p-4 bg-white rounded-md text-xs sm:text-sm md:text-base">
         {job.description}
       </p>
-      <div className="flex flex-col">
-        <h3 className="font-semibold border border-main p-2 rounded-md mb-2 text-main text-base sm:text-sm md:text-lg">
-          Category : {job.category}
+      <div className="flex flex-col mb-1">
+        <h3 className="font-semibold p-1 rounded-md text-main text-xs sm:text-sm md:text-base">
+          Category: {job.category}
         </h3>
-        <p className="border border-main p-2 rounded-md mb-2 text-base sm:text-sm md:text-lg">
-          Service : {job.service.join(' | ')}
-        </p>
-        <div className="flex items-center m-3">
-          <MapPin className="text-main" size={20} />
-          <span className="ml-2 text-base sm:text-sm md:text-lg">{job.location}</span>
+        <div className="flex flex-col sm:flex-row sm:justify-between items-start sm:items-center text-xs sm:text-sm md:text-base p-1 sm:p-2">
+          <p className="mb-1 sm:mb-0">
+            Service: {job.service.join(' | ')}
+          </p>
+          <div className="flex items-center mt-1 sm:mt-0">
+            <MapPin className="text-main" size={16} />
+            <span className="ml-1 text-xs sm:text-sm md:text-base">{job.location}</span>
+          </div>
         </div>
       </div>
     </div>
