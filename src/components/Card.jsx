977be77b0ca2bb299709f00ee19027aa7f3bff35
Mostapha Yasser <mostaphayasser18@yahoo.com>
2024-09-16 03:48:47 +0300

    dark-mode home

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,11 @@
 function Card({ title, description, icon: Icon, inverted = false }) {
     return (
       <div className={`p-6 rounded-lg shadow-md transition-all duration-300 hover:shadow-xl ${
-        inverted ? 'bg-main text-white' : 'bg-white text-gray-800 border border-gray-200 hover:border-main'
+        inverted ? 'bg-main text-white' : 'bg-white  text-text dark:text-text-dark dark:bg-light-dark-dark   hover:border-main'
       }`}>
         <Icon className={`w-12 h-12 mx-auto mb-4 ${inverted ? 'text-white' : 'text-main'}`} />
-        <h3 className="font-bold text-xl mb-2">{title}</h3>
-        <p className={inverted ? 'text-teal-50' : 'text-gray-600'}>{description}</p>
+        <h3 className="font-bold text-xl mb-2 dark:text-text-dark">{title}</h3>
+        <p className={inverted ? 'text-teal-50 dark:text-text-dark' : 'text-gray-600 dark:text-text-dark'}>{description}</p>
       </div>
     );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,9 +3,9 @@ import Card from './Card';
 
 function Section({ title, items }) {
   return (
-      <div className="py-16 bg-gray-50 dark:bg-gray-900">
+      <div className="py-16 bg-gray-50 dark:bg-transparent">
           <div className="container mx-auto px-4">
-              <h2 className="text-3xl font-bold mb-12 text-center text-gray-800 dark:text-gray-200">{title}</h2>
+              <h2 className="text-3xl font-bold mb-12 text-center text-main dark:text-accent">{title}</h2>
               <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                   {items.map((item, index) => (
                       <Card key={index} {...item} inverted={index % 2 === 0} />
