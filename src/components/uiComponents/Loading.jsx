48a97e562f4eaa43bd74be25ff1b58e659e01aaa
fatma-mohamed99 <fatma.99.mph@gmail.com>
2024-09-26 16:11:50 +0300

    a

--- src/components/uiComponents/Loading.jsx
+++ src/components/uiComponents/Loading.jsx
@@ -3,7 +3,7 @@ const Loading = () => {
     return (
       <div className=" flex flex-col items-center justify-center min-h-screen bg-white dark:bg-slate-700 ">
         <div className="w-16 h-16 border-t-4 border-r-4 border-b-4 dark:text-white border-main rounded-full animate-spin dark:border-white"></div>
-        <span className="mt-4 text-xl font-bold text-main dark:text-white ">Loading data...</span>
+        <span className="mt-4 text-xl font-bold text-main dark:text-white ">Please Wait ...</span>
         <div className="mt-2 w-48 h-1 bg-gray-200 rounded-full overflow-hidden">
           <div className="w-full h-full bg-main animate-pulse"></div>
         </div>
