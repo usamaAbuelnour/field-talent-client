dec718f849af9e1478aa490c25f82f7e091213c1
fatma-mohamed99 <fatma.99.mph@gmail.com>
2024-09-29 19:16:14 +0300

    client details & some ui in category

--- src/components/JobDetailsForApplyComponents/ClientDetails.jsx
+++ src/components/JobDetailsForApplyComponents/ClientDetails.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 
-const ClientDetails = () => {
+const ClientDetails = ({ user }) => {
   return (
     <div className="p-6 dark:bg-main-dark dark:bg-opacity-25 shadow-lg rounded-lg dark:text-white">
-      <h2 className="text-xl font-bold text-main mb-4  text-center dark:text-accent ">About Client</h2>
+      <h2 className="text-xl font-bold text-main mb-4 text-center dark:text-accent">About Client</h2>
       
       <div className="mb-4 dark:text-white">
         <p className="font-semibold bg-main text-white p-2 rounded m-2">Client Name</p> 
-        <p className="ml-2 p-2">Fatma Mohamed</p>
+        <p className="ml-2 p-2">{user.firstName} {user.lastName}</p>
       </div>
       <div className="mb-4 dark:text-white">
         <p className="font-semibold bg-main text-white p-2 rounded m-2">Joined</p> 
-        <p className="ml-2 p-2">1-10-2024</p>
-      </div>
-      <div className="mb-4 dark:text-white">
-        <p className="font-semibold bg-main text-white p-2 rounded m-2">Number of Offer Jobs</p> 
-        <p className="ml-2 p-2">15</p>
+        <p className="ml-2 p-2">{new Date(user.createdAt).toLocaleDateString()}</p>
       </div>
     </div>
   );
