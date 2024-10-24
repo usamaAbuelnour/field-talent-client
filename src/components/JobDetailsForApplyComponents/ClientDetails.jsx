937a5e2d7dd95d903545bf3f4b94f96b281f495f
fatma-mohamed99 <fatma.99.mph@gmail.com>
2024-10-24 23:23:57 +0300

    editUI

--- src/components/JobDetailsForApplyComponents/ClientDetails.jsx
+++ src/components/JobDetailsForApplyComponents/ClientDetails.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
 
 const ClientDetails = ({ user }) => {
   return (
-    <div className="p-6 dark:bg-main-dark dark:bg-opacity-25 shadow-lg rounded-lg dark:text-white">
-      <h2 className="text-xl font-bold text-main mb-4 text-center dark:text-accent">About Client</h2>
-      
-      <div className="mb-4 dark:text-white">
-        <p className="font-semibold bg-main text-white p-2 rounded m-2">Client Name</p> 
-        <p className="ml-2 p-2">{user.firstName} {user.lastName}</p>
+    <div className=" dark:-dark dark:bg-opacity-25   dark:text-white mt-16  p-2  bg-gradient-to-r from-green-50  to-slate-50 border rounded-md   border-x-0 mx-7 dark:bg-gradient-to-r dark:from-main  dark:to-main-dark">
+  <div className=" md:flex mx-auto w-fit">
+
+      <div  className="flex gap-1 sm:mr-2 md:mr-4 ">
+
+        <p className="font-semibold  text-slate-600 sm:text-sm md:text-xl  lg:text-xl dark:text-accent-dark">Client Name:</p>
+        <p className="sm:text-sm md:text-xl lg:text-xl dark:text-accent">{user.firstName} {user.lastName}</p>
       </div>
-      <div className="mb-4 dark:text-white">
-        <p className="font-semibold bg-main text-white p-2 rounded m-2">Joined</p> 
-        <p className="ml-2 p-2">{new Date(user.createdAt).toLocaleDateString()}</p>
+      <div  className="flex gap-1 ml-3 ">
+        <p className="font-semibold  text-slate-600 sm:text-sm md:text-xl lg:text-xl dark:text-accent-dark">Joined:</p>
+        <p className="sm:text-sm md:text-xl lg:text-xl dark:text-accent">{new Date(user.createdAt).toLocaleDateString()}</p>
+      </div>
       </div>
     </div>
   );
 };
 
-export default ClientDetails;
+export default ClientDetails ;
--- src/components/JobDetailsForApplyComponents/JobDetails.jsx
+++ src/components/JobDetailsForApplyComponents/JobDetails.jsx
@@ -3,7 +3,7 @@ import ApplyPopup from './ApplyPopup';
 import { useEffect, useState } from 'react';
 
 const FieldLoader = () => (
-  <div className="inline-block w-4 h-4 border-t-2 border-main rounded-full animate-spin ml-2"></div>
+  <div className="inline-block w-4 h-4 border-t-2 border-main rounded-full animate-spin ml-2 mt-4 "></div>
 );
 
 const JobDetails = ({ job }) => {
@@ -20,22 +20,21 @@ const JobDetails = ({ job }) => {
     window.scrollTo(0, 0); 
   }, []);
   return (
-    <div>
-      <h2 className="font-bold mb-10 text-2xl sm:text-xl md:text-3xl text-main text-center dark:text-accent">
+    <div className='mt-16  border-2 shadow-md rounded-lg border-t-0 dark:border-slate-800'>
+      
+      <h2 className="rounded-md bg-gradient-to-r from-green-50  to-slate-50 pb-1 mb-6 text-2xl  md:text-3xl text-main text-center dark:text-accent dark:bg-gradient-to-r dark:to-main  dark:from-main-dark ">
         {job.title}
       </h2>
 
-
-
-      <div className="mb-4 flex flex-col sm:flex-col md:flex-row justify-between items-start md:items-center mx-4  gap-2">
-        <div className="order-1 md:order-none text-lg sm:text-lg md:text-xl text-gray-400 dark:text-white ">
+      <div className=" flex flex-col  md:flex-row justify-between  md:items-center mx-4 items-center gap-2">
+        <div className="order-1 md:order-none text-lg  md:text-xl text-gray-400 dark:text-white ">
           Created: {job.createdAt ? new Date(job.createdAt).toLocaleDateString() : <FieldLoader />}
         </div>
-        <div className="order-3 md:order-none text-main flex items-center gap-2 dark:text-white">
-          <MapPin />
-          <span className="text-lg sm:text-xl md:text-2xl">{job.location}</span>
+        <div className="order-3 md:order-none  flex items-center gap-2 ">
+          <MapPin className='hidden md:inline text-main dark:text-white' /><p className='md:hidden text-lg text-main dark:text-white'>Location:</p>
+          <span className="text-lg  md:text-2xl md:text-main text-gray-600 dark:text-white ">{job.location}</span>
         </div>
-        <div className="order-2 md:order-none text-lg sm:text-lg md:text-xl text-gray-400 dark:text-white">
+        <div className="order-2 md:order-none text-lg  md:text-xl text-gray-400 dark:text-white">
           Updated: {job.updatedAt ? new Date(job.updatedAt).toLocaleDateString() : <FieldLoader />}
         </div>
       </div>
@@ -43,14 +42,14 @@ const JobDetails = ({ job }) => {
 
 
 
-      <p className="text-gray-700 my-11 dark:text-white text-base sm:text-lg md:text-xl mx-4 break-words">
-        Description: {job.description || <FieldLoader />}
+      <p className="text-gray-700 my-8 dark:text-white text-base  md:text-xl mx-2 break-words border-2 border-y-main border-x-0 shadow-sm p-2 pb-5">
+       <span className='block   bg-gradient-to-t from-slate-50 to-slate-100 rounded-md text-center mb-4 pb-2 dark:bg-gradient-to-r dark:to-transparent  dark:from-main-dark'> Description : </span> {job.description || <FieldLoader />}
       </p>
 
-      <div className="text-lg sm:text-lg md:text-xl font-semibold mb-2 dark:text-white text-main mx-4">
+      <div className="text-lg  md:text-xl font-semibold mb-2 dark:text-white text-main mx-4">
         Category: {job.category || <FieldLoader />}
       </div>
-      <div className="text-lg  mb-4 dark:text-white text-main mx-2 p-2  w-fit rounded-full">
+      <div className="text-lg  mb-2 dark:text-white text-main mx-2 p-2  w-fit rounded-full">
         Service: {job.service && job.service.length > 0 ? job.service.join('  |  ') : <FieldLoader />}
       </div>
 
@@ -64,6 +63,7 @@ const JobDetails = ({ job }) => {
       </div>
 
       {isPopupOpen && <ApplyPopup closePopup={closePopup} jobId={job._id} />}
+     
     </div>
   );
 };
