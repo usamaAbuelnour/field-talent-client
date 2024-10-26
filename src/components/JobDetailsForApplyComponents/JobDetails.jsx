a289f2aa5d5cf59dbd8e683813e784bc0a51241a
Mostapha Yasser <mostaphayasser18@yahoo.com>
2024-10-26 03:06:15 +0300

    fix: reject sytatue in ui 4

--- src/components/JobDetailsForApplyComponents/JobDetails.jsx
+++ src/components/JobDetailsForApplyComponents/JobDetails.jsx
@@ -1,9 +1,9 @@
-import { MapPin } from 'lucide-react';
+import { MapPin, ChevronLeft, ChevronRight } from 'lucide-react';
 import ApplyPopup from './ApplyPopup';
 import { useEffect, useState } from 'react';
 
 const FieldLoader = () => (
-  <div className="inline-block w-4 h-4 border-t-2 border-main rounded-full animate-spin ml-2 mt-4"></div>
+  <div className="inline-block w-4 h-4 border-t-2 border-main rounded-full animate-spin ml-2 mt-4 "></div>
 );
 
 const JobDetails = ({ job }) => {
@@ -16,43 +16,40 @@ const JobDetails = ({ job }) => {
   const closePopup = () => {
     setIsPopupOpen(false);
   };
-
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0); 
   }, []);
-
   return (
-    <div className="md:mt-16 mt-24 border-2 shadow-md rounded-lg border-t-0 dark:border-slate-800">
-      <h2 className="rounded-md bg-gradient-to-r from-green-50 to-slate-50 pb-1 mb-2 md:mb-6 text-2xl md:text-3xl text-main text-center dark:text-accent dark:bg-gradient-to-r dark:to-main dark:from-main-dark">
+    <div className='md:mt-16 mt-24 border-2 shadow-md rounded-lg border-t-0 dark:border-slate-800'>
+      
+      <h2 className="rounded-md bg-gradient-to-r from-green-50  to-slate-50 pb-1 mb-2 md:mb-6 text-2xl  md:text-3xl text-main text-center dark:text-accent dark:bg-gradient-to-r dark:to-main  dark:from-main-dark ">
         {job.title}
       </h2>
 
-      <div className="flex flex-col md:flex-row justify-between md:items-center mx-4 items-center gap-2">
-        <div className="text-lg md:text-xl text-gray-400 dark:text-white">
+      <div className=" flex flex-col  md:flex-row justify-between  md:items-center mx-4 items-center gap-2">
+        <div className="order-1 md:order-none text-lg  md:text-xl text-gray-400 dark:text-white ">
           Created: {job.createdAt ? new Date(job.createdAt).toLocaleDateString() : <FieldLoader />}
         </div>
-        <div className="flex items-center gap-2">
-          <MapPin className="hidden md:inline text-main dark:text-white" />
-          <span className="md:hidden text-lg text-main dark:text-white">Location:</span>
-          <span className="text-lg md:text-2xl text-gray-600 dark:text-white">{job.location}</span>
+        <div className="order-3 md:order-none  flex items-center gap-2 ">
+          <MapPin className='hidden md:inline text-main dark:text-white' /><p className='md:hidden text-lg text-main dark:text-white'>Location:</p>
+          <span className="text-lg  md:text-2xl md:text-main text-gray-600 dark:text-white ">{job.location}</span>
         </div>
-        <div className="text-lg md:text-xl text-gray-400 dark:text-white">
+        <div className="order-2 md:order-none text-lg  md:text-xl text-gray-400 dark:text-white">
           Updated: {job.updatedAt ? new Date(job.updatedAt).toLocaleDateString() : <FieldLoader />}
         </div>
       </div>
 
-      <p className="text-gray-700 my-8 dark:text-white text-base md:text-xl mx-2 break-words border border-y-slate-100 border-x-0 shadow-sm p-2 pb-5">
-        <span className="block bg-gradient-to-t from-slate-50 to-slate-100 rounded-md text-center mb-4 pb-2 dark:bg-gradient-to-r dark:to-transparent w-fit dark:from-main-dark">
-          Description
-        </span> 
-        {job.description || <FieldLoader />}
+
+
+
+      <p className="text-gray-700 my-8 dark:text-white text-base  md:text-xl mx-2 break-words border border-y-slate-100 border-x-0 shadow-sm p-2 pb-5">
+       <span className='block   bg-gradient-to-t from-slate-50 to-slate-100 rounded-md text-center mb-4 pb-2 dark:bg-gradient-to-r dark:to-transparent w-fit dark:from-main-dark'> Description  </span> {job.description || <FieldLoader />}
       </p>
 
-      <div className="text-lg md:text-xl font-semibold mb-2 dark:text-white text-main mx-4">
+      <div className="text-lg  md:text-xl font-semibold mb-2 dark:text-white text-main mx-4">
         Category: {job.category || <FieldLoader />}
       </div>
-
-      <div className="text-lg mb-2 dark:text-white text-main mx-2 p-2 w-fit rounded-full">
+      <div className="text-lg  mb-2 dark:text-white text-main mx-2 p-2  w-fit rounded-full">
         Service: {job.service && job.service.length > 0 ? job.service.join('  |  ') : <FieldLoader />}
       </div>
 
@@ -66,6 +63,7 @@ const JobDetails = ({ job }) => {
       </div>
 
       {isPopupOpen && <ApplyPopup closePopup={closePopup} jobId={job._id} />}
+     
     </div>
   );
 };
--- src/pages/JobDetailsForApply.jsx
+++ src/pages/JobDetailsForApply.jsx
@@ -1,19 +1,19 @@
 import { useLocation } from 'react-router-dom';
 import JobDetails from '../components/JobDetailsForApplyComponents/JobDetails';
-import ClientDetails from '../components/JobDetailsForApplyComponents/ClientDetails';
 import { Navigate } from "react-router-dom";
+import ClientDetails from './../components/JobDetailsForApplyComponents/ClientDetails';
 
 const JobDetailsForApply = () => {
   const location = useLocation();
   const job = location.state?.job;
 
   return (
-    <div className="relative z-10 container mx-auto my-10 p-6 overflow-x-hidden">
-      <div className="flex flex-col lg:flex-row gap-8 mt-9">
-        <div className="w-full lg:w-1/5 lg:fixed lg:top-1/2 lg:transform lg:-translate-y-1/2 order-2 lg:order-1">
+    <div className="relative container mx-auto my-10 p-6 overflow-x-hidden">
+      <div className="mt-9 ">
+        <div className=" absolute w-full md:-top-7 -top-11 -right-0">
           {job ? <ClientDetails user={job.userId} /> : null}
         </div>
-        <div className="w-full lg:w-4/5 order-1 lg:order-2 lg:ml-[23%]">
+        <div className="w-full mt-4 ">
           {job ? (
             <JobDetails job={job} />
           ) : (
